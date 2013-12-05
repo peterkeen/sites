738c88422ae871b58c712d514aabee578819efb6
Pete Keen <peter.keen@bugsplat.info>
2013-12-04 20:54:53 -0500

    Add caching

--- middleware.rb
+++ middleware.rb
@@ -1,9 +1,13 @@
+require 'time'
+require 'thread'
+
 module Sites
   class Middleware
     def initialize(app)
       @app = app
       @sites_manager = Sites::Manager.new
       @sites_viewer = Sites::Viewer.new
+      @cache_mutex = Mutex.new
     end
 
     def call(env)
@@ -46,16 +50,25 @@ module Sites
     end
 
     def cname_map
-      cnames = {}
-      @all_sites = Dir.glob("#{ENV['SITES_BASE_PATH']}/*.git").select { |fn| File.directory?(fn) }
-      @all_sites.each do |site|
-        site_name = site.gsub(ENV['SITES_BASE_PATH'], '').gsub('.git', '').gsub('/', '')
-        wiki = Gollum::Wiki.new(site, {})
-        names = wiki.page('cnames')
-        next unless names
-        cnames[site_name] = names.raw_data.split("\n")
+      now = Time.now.to_i
+      if (@cname_map_cache_expire_time || 0) >= now
+        return @cname_map_cache
+      else
+        cnames = {}
+        @all_sites = Dir.glob("#{ENV['SITES_BASE_PATH']}/*.git").select { |fn| File.directory?(fn) }
+        @all_sites.each do |site|
+          site_name = site.gsub(ENV['SITES_BASE_PATH'], '').gsub('.git', '').gsub('/', '')
+          wiki = Gollum::Wiki.new(site, {})
+          names = wiki.page('cnames')
+          next unless names
+          cnames[site_name] = names.raw_data.split("\n")
+        end
+        @cache_mutex.synchronize do
+          @cname_map_cache = cnames
+          @cname_map_cache_expire_time = now + 60
+        end
       end
-      cnames
+      @cname_map_cache
     end
   end
 end
--- viewer.rb
+++ viewer.rb
@@ -1,3 +1,5 @@
+require 'lru_redux'
+
 module Sites
 
   class Viewer < Sinatra::Base
@@ -6,18 +8,33 @@ module Sites
       Gollum::Wiki.new(ENV['SITES_BASE_PATH'] + env['SCRIPT_NAME'] + '.git', {})
     end
 
+    def initialize(app = nil)
+      super(app)
+      @cache = LruRedux::ThreadSafeCache.new(100)
+    end
+
+    def cache_key(wiki, key)
+      rev = wiki.repo.head.commit.id
+      "#{key}-#{rev}"
+    end
+
     get '/' do
       wiki = wiki_new
-      @page = wiki.page('Home')
+      key = cache_key(wiki, 'Home')
+      response.headers['X-Cache-Key'] = key
 
-      layout = wiki.file('layout.erb')
-      layout_data = if layout
-        layout.raw_data
-      else
-        "<%= yield %>"
+      @cache.getset(key) do
+        @page = wiki.page('Home')
+  
+        layout = wiki.file('layout.erb')
+        layout_data = if layout
+          layout.raw_data
+        else
+          "<%= yield %>"
+        end
+
+        render :erb, @page.formatted_data, layout: layout_data
       end
-
-      render :erb, @page.formatted_data, layout: layout_data
     end
 
     get '/cnames' do
@@ -26,24 +43,28 @@ module Sites
 
     get '/*' do
       wiki = wiki_new
-
       page_name = params[:splat][0].gsub(/\.html$/, '')
 
-      if (@page = wiki.page(page_name))
-        layout = wiki.file('layout.erb')
-        layout_data = if layout
-          layout.raw_data
+      key = cache_key(wiki, page_name)
+      response.headers['X-Cache-Key'] = key
+
+      @cache.getset(key) do
+        if (@page = wiki.page(page_name))
+          layout = wiki.file('layout.erb')
+          layout_data = if layout
+            layout.raw_data
+          else
+            "<%= yield %>"
+          end
+    
+          render :erb, @page.formatted_data, layout: layout_data
+        elsif (file = wiki.file(page_name))
+          mimetype = MIME::Types.of page_name
+          content_type mimetype[0]
+          return file.raw_data
         else
-          "<%= yield %>"
+          raise Sinatra::NotFound
         end
-  
-        render :erb, @page.formatted_data, layout: layout_data
-      elsif (file = wiki.file(page_name))
-        mimetype = MIME::Types.of page_name
-        content_type mimetype[0]
-        return file.raw_data
-      else
-        raise Sinatra::NotFound
       end
     end
 
